05d0e20a10293e4b860edad5bb9a91d9925e5876
naveenapius <naveenapius@protonmail.com>
2024-04-05 16:02:00 +0530

    insert data did not work on my system. Changed syntax to something a little more ubiquitous

--- database_initialiser.py
+++ database_initialiser.py
@@ -18,8 +18,9 @@ def initialiseTable(listing) :
   db = mysql.connector.connect(
       host="localhost",
       user="root",
-      password="password",
-      database="portfoliobroTest",
+      password="",
+      database="portfoliobro_test",
+      port="3307",
       buffered=True
       )
   
@@ -42,27 +43,27 @@ def initialiseTable(listing) :
   
   #if table exists insert values into it if empty. if table already populated display message
   if table_exists :
-    # Insert Dataframe into SQL Server:
-    sql1 = "INSERT INTO nifty_500 (companyName, industry, symbol, series, ISINCode, Beta, Volatility ) values(%s,%s,%s,%s,%s,%s,%s)"
-    val = ((Company["Company Name"], Company["Industry"], Company["Symbol"], Company["Series"], Company["ISIN Code"], Company["Beta"], Company["Volatility"],) for Company in listing)
+    # Insert Dataframe into SQL Server:  
     try :
-      cursor.executemany(sql1,val)
+      print("Attempting to populate table nifty_500")
+      for Company in listing:
+        insert_row = 'INSERT INTO nifty_500 values("{}","{}","{}","{}","{}",{},"{}")'.format(Company["Company Name"], Company["Industry"], Company["Symbol"], Company["Series"], Company["ISIN Code"], Company["Beta"], Company["Volatility"])
+        cursor.execute(insert_row)
     except mysql.connector.errors.IntegrityError :
-      print("Table population failed as table already populated with values")
+      print("Table population skipped. Table already populated with values")
     
    #if table does not exist, create table and populate it. 
   else :
-      print("Creating table...")
-      sql2="CREATE TABLE nifty_500 (companyName varchar(250), industry varchar(50), symbol varchar(15), series varchar(5),  ISINCode varchar(20), Beta decimal(4,2), Volatility varchar(15), CONSTRAINT pk_nifty PRIMARY KEY(symbol))"
+      print("Attempting to create table")
       try :
-        cursor.execute(sql2)
+        table_create_query="CREATE TABLE nifty_500 (companyName varchar(250), industry varchar(50), symbol varchar(15), series varchar(5),  ISINCode varchar(20), Beta decimal(4,2), Volatility varchar(15), CONSTRAINT pk_nifty PRIMARY KEY(symbol))"
+        cursor.execute(table_create_query)
         print("Table created successfully")
       except : 
          print("Table could not be created")
-
-      sql1 = "INSERT INTO nifty_500 (companyName, industry, symbol, series, ISINCode, Beta, Volatility ) values(%s,%s,%s,%s,%s,%s,%s)"
-      val = ((Company["Company Name"], Company["Industry"], Company["Symbol"], Company["Series"], Company["ISIN Code"], Company["Beta"], Company["Volatility"],) for Company in listing)
-      cursor.executemany(sql1,val)
+      for Company in listing:
+        insert_row = 'INSERT INTO nifty_500 values("{}","{}","{}","{}","{}",{},"{}")'.format(Company["Company Name"], Company["Industry"], Company["Symbol"], Company["Series"], Company["ISIN Code"], Company["Beta"], Company["Volatility"])
+        cursor.execute(insert_row)
       
   db.commit()
   cursor.close()
