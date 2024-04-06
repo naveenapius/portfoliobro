7dddd331007369ea7158882660620be7cc2c7051
kay-zee-yah <keziahsymes@hotmail.com>
2024-04-06 17:04:52 +0530

    modifying beta updation to update price

--- beta_updation.py
+++ beta_updation.py
@@ -19,12 +19,20 @@ for Company in Listing:
     stock_name=Company["Symbol"]
     stock_name = f"{stock_name}.NS" if ".NS" not in stock_name else stock_name
     print("Fetching data for stock : ",stock_name)
+    stock = yf.Ticker(stock_name)
+    current_price = stock.info['currentPrice']
+    Company['Price'] = current_price
     Company['Beta'] = beta.calcBeta(stock_name,index_df) 
     Company['Volatility'] = beta.checkVolatility(Company['Beta'])
 
 #print updated values for debugging purposes : can remove
 for Company in Listing:
-	print(Company['Company Name'], " - ", Company["Symbol"], " - ", Company["Beta"]," - " ,Company["Volatility"])
+	print(Company['Company Name'], " - ", Company["Symbol"], " - ",Company["Price"]," - ", Company["Beta"]," - " ,Company["Volatility"])
+
+#rewrite json with updated values
+with open('database.json', 'w') as json_file:
+  json_file.seek(0)
+  json.dump(Listing, json_file, indent=4)
 
 #push to database
 update.updateDatabase(Listing)
