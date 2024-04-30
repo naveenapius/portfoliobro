2f40ce05bbd698d6712a530633026e79a81e406e
naveenapius <naveenapius@protonmail.com>
2024-04-30 13:34:31 +0530

    validations

--- main.py
+++ main.py
@@ -6,24 +6,71 @@ import visualiser as vis
 LOGIN_STATUS = 0 # latch for login management
 
 
-def simulatorHelper(uname):
-    portfolio = dbh.getPortfolio(uname)
+
+# MENUS
+def showLoginMenu():
+    print("\n\nAvailable actions: ")
+    print("l - Login")
+    print("s - Signup")
+    print("q - Quit\n")
+      
+
+def showLoggedInMenu():
+    print("\n\nAvailable actions:")
+    print("u - Update portfolio")
+    print("v - Visualise portfolio")
+    print("s - Simulate additions/removals")
+    print("r - Get recommendation")
+    print("l - Logout")
+    print("q - Quit\n")
+
+
+def showVisualiserMenu():
+    print("\nAvailable actions: ")
+    print("p - Visualise portfolio")
+    print("b - Visualise beta")
+    print("r - Visualise risk/return profile\n")
+
+def showSimulatorMenu():
     print("\nAvailable actions: ")
     print("a - Simulate addition")
     print("r - Simulate removal")
+def simulatorHelper(uname):
+    portfolio = dbh.getPortfolio(uname)
+    while(True):
+        o = input("Action>> ")
+        if o=='a':
+            flag=0
+            stock = input("Enter stock to simulate: ")
+            vol = int(input("Enter volume: "))
+            return sim.simulate(portfolio, stock, vol, flag)
+        else:
+            print("Invalid option. Please try again.")
+            showSimulatorMenu()
 
-    o = input("Action>> ")
-    flag = 0 if o=='a' else '1'
-    stock = input("Enter stock to simulate: ")
-    vol = int(input("Enter volume: "))
-
-    print("New portfolio beta: ",sim.simulate(portfolio, stock, vol, flag))
-    return
 
 
+# VALIDATORS
 
+def validateUname(uname):
+    """
+    :param uname: entered username to be validated
+    :return : 0/1 validation
+    """
+    if len(uname.split() > 1):
+        return [0, "spaces not allowed in username"]
+    for i in uname:
+        if not(i.isalnum()) or not(i=="_"):
+            return [0, ""]
 
+def validatePassword(pwd):
+    pass
 
+def validateLegalName(legal_name):
+    pass
+
+def validatePhoneNumber(phone):
+    pass
 
 
 def uLogin():
@@ -39,6 +86,7 @@ def uLogin():
         return 0
 
 
+# HELPERS
 def getRisk(opt):
     if opt == 'vl':
         return "very low"
@@ -103,81 +151,62 @@ def updatePortfolio(uname):
     return
 
 
-def showLoginMenu():
-    print("\n\nAvailable actions: ")
-    print("l - Login")
-    print("s - Signup")
-    print("q - Quit\n")
-    
-    
-
-def showLoggedInMenu():
-    print("\n\nAvailable actions:")
-    print("u - Update portfolio")
-    print("v - Visualise portfolio")
-    print("s - Simulate additions/removals")
-    print("r - Get recommendation")
-    print("l - Logout")
-    print("q - Quit\n")
 
 
-def showVisualiserMenu():
-    print("\nAvailable actions: ")
-    print("p - Visualise portfolio contents")
-    print("b - Visualise beta of portfolio")
-    print("r - Visualise risk/return of portfolio\n")
 
-#main
-while True:
-    if LOGIN_STATUS==0:
-        showLoginMenu()
-        opt = input("Action>> ")
-        if opt=='l':
-            r = uLogin()
-            LOGIN_STATUS = 1 if r==1 else 0
-        elif opt=='s':
-            uSignUp()
-        elif opt=='q':
-            print("Bye!")
-            exit()
-        else:
-            print("Invalid option. Please try again.")
-    elif LOGIN_STATUS==1:
-        showLoggedInMenu()
-        opt = input("Action>> ")
-        if opt=='u':
-            updatePortfolio(user_name)
-        elif opt=='v':
-            portfolio = dbh.getPortfolio(user_name)
-            showVisualiserMenu()
+
+
+if __name__ == '__main__':
+    while True:
+        if LOGIN_STATUS==0:
+            showLoginMenu()
             opt = input("Action>> ")
-            if opt=='p':
-                vis.weightedPortfolioVisualisation(portfolio, user_name)
-            elif opt=='b':
-                vis.betaVisualisation(portfolio, user_name)
+            if opt=='l':
+                r = uLogin()
+                LOGIN_STATUS = 1 if r==1 else 0
+            elif opt=='s':
+                uSignUp()
+            elif opt=='q':
+                print("Bye!")
+                exit()
+            else:
+                print("Invalid option. Please try again.")
+        elif LOGIN_STATUS==1:
+            showLoggedInMenu()
+            opt = input("Action>> ")
+            if opt=='u':
+                updatePortfolio(user_name)
+            elif opt=='v':
+                portfolio = dbh.getPortfolio(user_name)
+                showVisualiserMenu()
+                opt = input("Action>> ")
+                if opt=='p':
+                    vis.weightedPortfolioVisualisation(portfolio, user_name)
+                elif opt=='b':
+                    vis.betaVisualisation(portfolio, user_name)
+                elif opt=='r':
+                    vis.riskReturnVisualisation(portfolio,user_name)
+            elif opt=='s':
+                print("New portfolio beta: ", simulatorHelper(user_name))
+            elif opt=='l':
+                LOGIN_STATUS = 0
+                print("User logged out successfully!")
             elif opt=='r':
-                vis.riskReturnVisualisation(portfolio,user_name)
-        elif opt=='s':
-            simulatorHelper(user_name)
-        elif opt=='l':
-            LOGIN_STATUS = 0
-            print("User logged out successfully!")
-        elif opt=='r':
-            risk_app = dbh.getRiskAppetite(user_name)
-            portfolio = dbh.getPortfolio(user_name)
-            suggested = (ss.suggestions(portfolio, risk_app))
-            print("Suggestions")
-            for i, stocks in suggested.items():
-                print("---------------------------")
-                print(i)
-                print("---------------------------")
-                for j in stocks:
-                    for k in j:
-                        print(k[0])
+                risk_app = dbh.getRiskAppetite(user_name)
+                portfolio = dbh.getPortfolio(user_name)
+                suggested = (ss.suggestions(portfolio, risk_app))
+                print("Suggestions")
+                for i, stocks in suggested.items():
+                    print("---------------------------")
+                    print(i)
+                    print("---------------------------")
+                    for j in stocks:
+                        for k in j:
+                            print(k[0])
 
-        elif opt=='q':
-            print("Automatic user logout") 
-            print("Bye!")
-            exit()
-        else:
-            print("Invalid option. Please try again.")
+            elif opt=='q':
+                print("Automatic user logout") 
+                print("Bye!")
+                exit()
+            else:
+                print("Invalid option. Please try again.")
